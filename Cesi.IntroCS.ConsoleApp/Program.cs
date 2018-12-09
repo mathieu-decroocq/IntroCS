bb366c67588cf1b661ebb23632784bb7e1373d30
Mathieu DECROOCQ <mathieu.decroocq@live.fr>
2018-12-09 13:28:06 +0100

    add async exemple in console

--- Cesi.IntroCS.ConsoleApp/Program.cs
+++ Cesi.IntroCS.ConsoleApp/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cesi.IntroCS.ConsoleApp
@@ -20,10 +22,15 @@ namespace Cesi.IntroCS.ConsoleApp
             //ExempleStringFormat(userName, now);
             //ExempleStringBuilder();
 
+            ExempleHttpClient().Wait();
+
+            ExempleTaskRun().Wait();
+
             Console.WriteLine("Pressez n'importe quelle touche pour quitter");
             Console.ReadKey();
         }
 
+        #region StringFormat Exemple
         private static void ExempleBasicConcat(string userName, DateTime now)
         {
             string output = "Bonjour " + userName + ". Nous sommes en " + now.Year + " et il est " + now.ToShortTimeString() + ".";
@@ -51,8 +58,39 @@ namespace Cesi.IntroCS.ConsoleApp
             Console.WriteLine(outputV2);
             Console.WriteLine(outputV3);
         }
+        #endregion
 
+        #region Async Exemple
 
+        private static async Task ExempleHttpClient()
+        {
+            HttpClient client = new HttpClient();
+
+            string data = await client.GetStringAsync("https://anapioficeandfire.com/api/characters/583");
+
+            Console.WriteLine(data);
+        }
+
+        private static async Task ExempleTaskRun()
+        {
+            ShowThreadInfo("Application");
+
+            await Task.Run(() =>
+            {
+                ShowThreadInfo("Task");
+            });
+
+            // The example displays the following output:
+            //       Application thread ID: 1
+            //       Task thread ID: 3
+        }
+
+        private static void ShowThreadInfo(String s)
+        {
+            Console.WriteLine("{0} Thread ID: {1}", s, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        #endregion
 
 
 
